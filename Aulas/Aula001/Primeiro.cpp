0e44b11305c2b80de57f1e8d5c2d1de5a6f2ff02
macielthiago <maciel.rosari54@gmail.com>
2025-12-07 08:10:32 -0400

    Initial commit

---
+++ Aulas/Aula001/Primeiro.cpp
@@ -0,0 +1,11 @@
+// prmeiro.cpp - mostra uma mensagem
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Bem vindo ao C++";
+	cout << endl;
+	cout << "Meu primeiro programa!" << endl;
+
+	return 0;
+}
