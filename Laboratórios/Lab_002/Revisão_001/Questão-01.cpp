89d212fc7cb07c6ac6159d90fff83ab2617a5da3
macielthiago <maciel.rosari54@gmail.com>
2025-12-16 18:27:48 -0400

    Exercício de revisão

---
+++ Laboratórios/Lab_002/Revisão_001/Questão-01.cpp
@@ -0,0 +1,9 @@
+// Exercício de revisão 001
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bem vindo" << endl << "ao mundo" << endl << "do C++" << endl;
+	return 0;
+}
---
+++ Laboratórios/Lab_002/Revisão_002/Questão-02.cpp
@@ -0,0 +1,13 @@
+// exercício de revisão 02
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bem vindo" << endl;
+	cout << "ao mundo" << endl;
+	cout << "do C++" << endl;
+
+	return 0;
+}
