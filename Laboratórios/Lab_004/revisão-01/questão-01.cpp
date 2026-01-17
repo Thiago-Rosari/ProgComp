9c4e7c67eb6787c4e0375dcc362ddc93e972eab5
macielthiago <maciel.rosari54@gmail.com>
2026-01-17 16:17:41 -0400

    Exercícios de revisão

---
+++ Laboratórios/Lab_004/revisão-01/questão-01.cpp
@@ -0,0 +1,15 @@
+// Exercício de revisão 01
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	srand(8);
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+
+	return 0;
+}
---
+++ Laboratórios/Lab_004/revisão-02/questão-02.cpp
@@ -0,0 +1,16 @@
+// Exercício de revisão 02
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+
+	return 0;
+}
