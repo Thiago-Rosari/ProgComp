72d2c44832a34548d198adc5bdefbc9ef8cf3b02
macielthiago <maciel.rosari54@gmail.com>
2026-03-18 18:36:44 -0400

    bg

---
+++ Laboratórios/Lab_005/Fixação-05/questão-01.cpp
@@ -0,0 +1,21 @@
+// Exercício de fixação 01
+#include <iostream>
+using namespace std;
+
+void temp(float);
+
+int main()
+{
+	cout << "Digite uma temperatura em graus celsius: " << endl;
+	float celsius;
+	cin >> celsius;
+
+	temp(celsius);
+	
+}
+
+void temp(float graus)
+{
+	float Fah = 1.8 * graus + 32.0;
+	cout << graus << " graus Celsius equivalem a " << Fah << " graus Fahrenheit." << endl;
+}
