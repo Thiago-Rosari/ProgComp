70848d4ecbda48bd592985a0e6fb12f5d9e7eae5
macielthiago <maciel.rosari54@gmail.com>
2026-06-21 09:25:15 -0400

    Exclusão das aulas e laboratórios

--- Laboratórios/Lab_002/Revisão_001/Questão-01.cpp
+++
@@ -1,9 +0,0 @@
-// Exercício de revisão 001
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Bem vindo" << endl << "ao mundo" << endl << "do C++" << endl;
-	return 0;
-}
--- Laboratórios/Lab_002/Revisão_005/Questão-05.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char ** argv)
-{
-	cout << argv[1] << " " << argv[2] << " programar!" << endl;
-
-	return 0;
-}
