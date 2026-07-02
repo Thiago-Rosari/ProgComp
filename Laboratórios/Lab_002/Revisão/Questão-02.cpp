b3bdb3a1017ecb0533954345687cb5e559b923c1
macielthiago <maciel.rosari54@gmail.com>
2026-07-02 18:26:01 -0400

    teste 3

---
+++ Laboratórios/Lab_002/Revisão/Questão-02.cpp
@@ -0,0 +1,10 @@
+// Exercício de revisão 02
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bem-vindo ";
+	cout << "ao mundo ";
+	cout << "do C++" << endl;
+}
---
+++ Laboratórios/Lab_002/Revisão/Questão-03.cpp
@@ -0,0 +1,18 @@
+// Exercício de revisão 03
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// exibindo uma frase que contém acentos
+	cout << "Acentuação é possível?" << endl;
+
+	// leia uma palavra que contém acento
+	char palavra[40];
+	cin >> palavra;
+
+	// exibindo oque foi lido
+	cout << palavra << endl;
+
+	return 0;
+}
